d17c182a5cf29fbc21d1a837ac706e96e076864d
Patrik <pg.patrik@gmail.com>
2020-02-04 10:51:46 +0100

    trying ti filter

---
+++ src/main/java/CollectionCommands.java
@@ -0,0 +1,79 @@
+import com.mongodb.client.*;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Projections;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Projections.fields;
+import static com.mongodb.client.model.Updates.inc;
+
+public class CollectionCommands {
+
+    public void printCollection(MongoCollection<Document> collection){
+        MongoCursor<Document> cursor = collection.find().iterator();
+        try {
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
+    }
+    public void printCafeFromCollection(MongoCollection<Document> collection){
+        AggregateIterable<Document> result = collection.aggregate(
+                Arrays.asList(
+                        Aggregates.match(Filters.eq("categories", "cafe")),
+                        Aggregates.project(
+                                fields(
+                                        Projections.excludeId(),
+                                        Projections.include("name")
+                                )
+                        )
+                )
+        );
+        List<Document> listDoc = new ArrayList<>();
+        for (Document doc : result) {
+            listDoc.add(doc);
+        }
+        for (int i = 0; i < listDoc.size(); i++) {
+            System.out.println(listDoc.get(i).toJson());
+        }
+    }
+
+    public void incrementStarsInCollection(MongoCollection<Document> collection){
+
+        collection.updateMany(eq("name", "XYZ Coffee Bar"), inc("stars", 1));
+    }
+
+    public void editNameInCollection(MongoCollection<Document> collection){
+
+        collection.updateOne(eq("name", "456 Cookies Shop"), new Document("$set", new Document("name", "123 Cookies  Heaven")));
+    }
+
+    public void aggregateInCollection(MongoCollection<Document> collection) {
+
+        AggregateIterable<Document> result = collection.aggregate(
+                Arrays.asList(
+                        Aggregates.match(Filters.gt("stars", 3)),
+                        Aggregates.project(
+                                fields(
+                                        Projections.excludeId(),
+                                        Projections.include("name","stars")
+                                )
+                        )
+                )
+        );
+        List<Document> listDoc = new ArrayList<>();
+        for (Document doc : result) {
+            listDoc.add(doc);
+        }
+        for (int i = 0; i < listDoc.size(); i++) {
+            System.out.println(listDoc.get(i).toJson());
+        }
+    }
+}
--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -13,14 +13,32 @@ import java.util.List;
 public class Program {
     public static void main(String[] args) throws FileNotFoundException {
 
+        OrgJsonMain json = new OrgJsonMain();
+        CollectionCommands cc = new CollectionCommands();
+
         MongoClient client = MongoClients.create();
         MongoDatabase database = client.getDatabase("javatest");
-        database.createCollection("coll");
-        MongoCollection<Document> collection = database.getCollection("coll");
-        OrgJsonMain json = new OrgJsonMain();
+        database.createCollection("coll2");
+        MongoCollection<Document> collection = database.getCollection("coll2");
+
         List<Document> docs = json.parseAndReturn("C:\\Users\\pgpat\\IdeaProjects\\Mongodb\\docs.json");
         collection.insertMany(docs);
 
+        System.out.println("\n" + "\n" + "Task2: Method for sys.out name on documents with category Cafe, " +
+                " only show the name of cafe's");
+        cc.printCafeFromCollection(collection);
+
+        System.out.println("\n" + "\n" + "Task3: Metod that increment “stars” for the restaurant\n" +
+                " with “name” = “XYZ Coffee Bar” with 1");
+        cc.incrementStarsInCollection(collection);
+        cc.printCollection(collection);
+
+        System.out.println("\n" + "\n" + "Task4: Method that updats “name” for “456 Cookies Shop“ to “123 Cookies  Heaven” ");
+        cc.editNameInCollection(collection);
+        cc.printCollection(collection);
+
+        System.out.println("\n" + "\n" + "Task5 Method aggragate restaurants with “stars” grater than 4 and print only  “name” and “stars” ");
+        cc.aggregateInCollection(collection);
         client.close();
     }
 }
